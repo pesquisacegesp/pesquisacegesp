098465cd1a32b534a393d9911b771840b67d1d46
pesquisacegesp <pmmacegesp@gmail.com>
2024-09-26 09:40:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,38 @@ function toggleFaltas() {
 document.getElementById("meuFormulario").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevenir o envio padrão do formulário
 
-    var form = document.getElementById("meuFormulario");
-    var formData = new FormData(form);
+    var unidade = document.getElementById("unidade").value; // Obter o valor da unidade
+    var errorMessage = document.getElementById("error-message"); // Área da mensagem de erro
 
-    fetch(form.action, {
-        method: "POST",
-        body: formData,
-        mode: "no-cors" // Para evitar problemas de CORS
-    })
-    .then(function() {
-        var modal = new bootstrap.Modal(document.getElementById("successModal"));
-        modal.show(); // Mostrar o modal de sucesso
-        form.reset(); // Resetar o formulário após o envio
-    })
-    .catch(function(error) {
-        console.error("Erro ao enviar dados:", error);
-    });
+    // Verificar se a Unidade já existe na planilha
+    fetch(`https://script.google.com/macros/s/AKfycbzxH1QzoA16XvqdkmsZkFDApZAs6URHdhtq2lqexsL3Tmv3MfdWUd2rdXHgO2tubDwS/exec?unidade=${unidade}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === "exists") {
+                // Se a Unidade já existe, exibe uma mensagem de erro
+                errorMessage.style.display = "block";
+            } else {
+                // Se a Unidade não existe, prossegue com o envio do formulário
+                errorMessage.style.display = "none";
+                var form = document.getElementById("meuFormulario");
+                var formData = new FormData(form);
+
+                fetch(form.action, {
+                    method: "POST",
+                    body: formData,
+                    mode: "no-cors" // Para evitar problemas de CORS
+                })
+                .then(function() {
+                    var modal = new bootstrap.Modal(document.getElementById("successModal"));
+                    modal.show(); // Mostrar o modal de sucesso
+                    form.reset(); // Resetar o formulário após o envio
+                })
+                .catch(function(error) {
+                    console.error("Erro ao enviar dados:", error);
+                });
+            }
+        })
+        .catch(function(error) {
+            console.error("Erro ao verificar duplicidade:", error);
+        });
 });
